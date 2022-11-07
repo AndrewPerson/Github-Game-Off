c0fa3497a2960abae21e79c7c7fa6a0080ae2943
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2022-11-08 07:49:15 +1100

    Changed to new namespace format

--- NLP/NLP.cs
+++ NLP/NLP.cs
@@ -6,38 +6,37 @@ using System.Linq;
 using System;
 using System.Threading.Tasks;
 
-namespace NLP
+namespace NLP;
+
+static class Word2VecImproved
 {
-    static class Word2VecImproved
+    public static float PhraseSimilarity(this Word2Vec self, string[] p1, string[] p2)
     {
-        public static float PhraseSimilarity(this Word2Vec self, string[] p1, string[] p2)
-        {
-            var v1 = p1.Select(w => self[w]).Aggregate((a, b) => a + b) / p1.Length;
-            var v2 = p2.Select(w => self[w]).Aggregate((a, b) => a + b) / p2.Length;
+        var v1 = p1.Select(w => self[w]).Aggregate((a, b) => a + b) / p1.Length;
+        var v2 = p2.Select(w => self[w]).Aggregate((a, b) => a + b) / p2.Length;
 
-            return (v1.VectorDot(v2) / MathF.Sqrt(v1.Sum(x => x * x)) * MathF.Sqrt(v2.Sum(x => x * x)) + 1) / 2;
-        }
+        return (v1.VectorDot(v2) / MathF.Sqrt(v1.Sum(x => x * x)) * MathF.Sqrt(v2.Sum(x => x * x)) + 1) / 2;
     }
+}
 
-    static class Normaliser
+static class Normaliser
+{
+    static readonly ILemmatizer lemmatiser = LemmatizerStore.Get(Language.English);
+
+    public static async Task<string[]> NormalisePhrase(string phrase)
     {
-        static readonly ILemmatizer lemmatiser = LemmatizerStore.Get(Language.English);
+        var nlp = await Pipeline.ForAsync(Language.English);
+        var doc = new Document(phrase, Language.English);
+        nlp.ProcessSingle(doc);
 
-        public static async Task<string[]> NormalisePhrase(string phrase)
-        {
-            var nlp = await Pipeline.ForAsync(Language.English);
-            var doc = new Document(phrase, Language.English);
-            nlp.ProcessSingle(doc);
-
-            return doc.ToTokenList().Where(t => t.POS == PartOfSpeech.ADJ ||
-                                                t.POS == PartOfSpeech.ADV ||
-                                                t.POS == PartOfSpeech.NOUN ||
-                                                t.POS == PartOfSpeech.VERB)
-                                    .Select(t => lemmatiser.GetLemma(t))
-                                    .Select(w => new string(w.ToLower()
-                                                             .Where(c => char.IsLetterOrDigit(c))
-                                                             .ToArray()))
-                                    .ToArray();
-        }
+        return doc.ToTokenList().Where(t => t.POS == PartOfSpeech.ADJ ||
+                                            t.POS == PartOfSpeech.ADV ||
+                                            t.POS == PartOfSpeech.NOUN ||
+                                            t.POS == PartOfSpeech.VERB)
+                                .Select(t => lemmatiser.GetLemma(t))
+                                .Select(w => new string(w.ToLower()
+                                                            .Where(c => char.IsLetterOrDigit(c))
+                                                            .ToArray()))
+                                .ToArray();
     }
 }
