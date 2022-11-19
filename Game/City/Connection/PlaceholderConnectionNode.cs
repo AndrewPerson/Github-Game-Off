3e047e5ff05de39d12e0eaab6b5dbff84b4a0c9c
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2022-11-19 14:01:53 +1100

    Added placeholder connection

---
+++ Game/City/Connection/PlaceholderConnectionNode.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+namespace Game;
+
+public partial class PlaceholderConnectionNode : ColorRect
+{
+    private Vector2 from = Vector2.Zero;
+	public Vector2 From
+    {
+        get => from;
+        set
+        {
+            from = value;
+            UpdatePosition();
+        }
+    }
+
+    private Vector2 to = Vector2.Zero;
+    public Vector2 To
+    {
+        get => to;
+        set
+        {
+            to = value;
+            UpdatePosition();
+        }
+    }
+
+	public override void _Ready()
+	{
+		UpdatePosition();
+	}
+
+    private void UpdatePosition()
+    {
+        float dist = from.DistanceTo(to);
+        Size = new Vector2(dist, Size.y);
+        Position = from + new Vector2(0, -PivotOffset.y);
+        Rotation = (from - to).Angle();
+
+        ((ShaderMaterial)Material).SetShaderParameter("size", new Vector2(dist / Size.y, 1));
+    }
+}
